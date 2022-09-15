0d8f255fa925c0ab56f2a5bb61b450e6905326b8
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-09-15 10:32:30 +0200

    updates

--- src/main/java/com/example/bmicalculator/Controller/BMIController.java
+++ src/main/java/com/example/bmicalculator/Controller/BMIController.java
@@ -1,15 +1,11 @@
 package com.example.bmicalculator.Controller;
 
-import com.example.bmicalculator.Model.DTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.text.DecimalFormat;
 
 @Controller
 public class BMIController {
@@ -30,13 +26,13 @@ public class BMIController {
         @GetMapping("/calculate")
         public String calculationPage(@RequestParam("userweight") double weight, @RequestParam("userheight") int height, Model model){
 
-            double result = (100*100*weight / (height * height));
-            result=Math.round(result);
+            double bmi = (10000*weight / (height * height));
+            bmi =Math.round(bmi);
 
-            model.addAttribute("result",result);
+            model.addAttribute("result", bmi);
             model.addAttribute("userweight", weight);
             model.addAttribute("userheight", height);
-            System.out.println(result);
+            System.out.println(bmi);
             return "calculation";
         }
 
